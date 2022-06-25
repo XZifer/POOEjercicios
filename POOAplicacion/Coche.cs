243ac5a72a6d32cd014020e344ca3e7830b56250
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-25 12:44:43 -0500

    Clase coche creada y metodo ToString Creado

---
+++ POOAplicacion/Coche.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOAplicacion
+{
+    public class Coche
+    {
+        private int id { get; set; }
+        public string marca { get; set; }
+        public string modelo { get; set; }
+        public int km { get; set; }
+        public double precio { get; set; }
+
+        public string ToString()
+        {
+            return "Detalles\n"+Marca+"\n"+Modelo+"\n"+km+"kms\n"+"$"+precio;
+        }
+    }
+}
