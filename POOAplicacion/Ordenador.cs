34ef1376013acaf17d190349fc65da30bfea68b1
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-26 09:09:54 -0500

    Clase Ordenador sus atributos y metodos encender y apagar

---
+++ POOAplicacion/Ordenador.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace POOAplicacion
+{
+    internal class Ordenador
+    {
+        public int tamañoDisco { get; set; }
+        public int tamañoDiscoMax { get; set; }
+        public bool encendido { get; set; }
+
+        public void encender()
+        {
+            encendido = true;
+        }
+        public void apagar()
+        {
+            encendido = false;
+        }
+    }
+}
