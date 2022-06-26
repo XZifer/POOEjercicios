8ff76016b74d757be442766a4c6bdb89ef3ec996
XZifer <lic.luisfsanchezbarrera@gmail.com>
2022-06-26 10:26:47 -0500

    Metodos AñadirDatos y EliminarDatos

--- POOAplicacion/Ordenador.cs
+++ POOAplicacion/Ordenador.cs
@@ -8,9 +8,20 @@ namespace POOAplicacion
 {
     internal class Ordenador
     {
-        public int tamañoDisco { get; set; }
-        public int tamañoDiscoMax { get; set; }
+        public int tamañoDiscoGB { get; set; }
+        public int tamañoDiscoMaxGB { get; set; }
         public bool encendido { get; set; }
+
+        private const int tamañoPorDefecto = 100;
+        public Ordenador()
+        {
+            tamañoDiscoGB = 0;
+            encendido = false;
+            if (tamañoDiscoMaxGB <= tamañoDiscoGB)
+            {
+                tamañoDiscoMaxGB = tamañoPorDefecto;
+            }
+        }
 
         public void encender()
         {
@@ -19,6 +30,31 @@ namespace POOAplicacion
         public void apagar()
         {
             encendido = false;
+        }
+        public void AñadirDatos(int datos)
+        {
+            if (encendido)
+            {
+                if ((tamañoDiscoGB + datos) > tamañoDiscoMaxGB)
+                {
+                    tamañoDiscoGB = tamañoDiscoMaxGB;
+                }
+                else
+                {
+                    tamañoDiscoGB += datos;
+                }
+            }
+        }
+        public void EliminarDatos(int datos)
+        {
+            if (datos > tamañoDiscoGB)
+            {
+                tamañoDiscoGB = 0;
+            }
+            else
+            {
+                tamañoDiscoGB -= datos;
+            }
         }
     }
 }
